c7561057c96566652ee608d3b113e1dd5da1642b
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-11-30 18:42:22 +0800

    Add files via upload

---
+++ Bill Payment Exam/Program(13) (1).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        BillPaymentSystem billSystem = new BillPaymentSystem();
+
+        billSystem.AddToBillQueue(123);
+        billSystem.AddToBillQueue(456);
+        billSystem.AddToBillQueue(789);
+
+        billSystem.ProcessBillPayments();
+        billSystem.ViewBillPaymentStack();
+
+        billSystem.CompleteBillPayments();
+        billSystem.ViewBillPaymentStack();
+
+   }
+}
