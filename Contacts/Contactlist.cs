6dff0bc51b7c6a1fb416b303da6b5339627bfb42
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-12-09 17:19:07 +0800

    Added comments

--- Contacts/Contactlist.cs
+++ Contacts/Contactlist.cs
@@ -4,32 +4,35 @@ using System.Diagnostics.Contracts;
 using System.Linq;
 
 public class Contactlist{
+    // The Contactlist object will contain a List of Contacts from the Contact object.
+    // Each entry in the List will have a name, order, email, and number associated with it.
     private List<Contact> contacts;
 
 
 
-
+    // Constructor for the Contactlist object.
     public Contactlist()
     {
         contacts = new List<Contact>();
     }
 
-
+    
+    // Function that will Add(add an index) to the contacts List.
+    // Accepts a name, an order, an email, and a number.
     public void add_name(string name, int order, string email, string number)
     {
         contacts.Add(new Contact(name, order, email, number));
     }
 
-
+    
     public void display_Contacts()
     {
-        System.Console.WriteLine("====== Contact List ====== ");    
+        // Function to display the entire contactlist.
+        System.Console.WriteLine("====== Contact List ====== ");   
+       
+        // the first four code e.g {0, -3}, {1, -20}, etc. are called "Padding", it will take the first variable "0" after which is "Order" etc.
         System.Console.WriteLine("{0, -3}\t{1,-20}\t   {2,-30}\t   {3,-15}","Order", "Name", "E-mail", "Number");
-        // foreach(Contact item in contacts)
-        // {
-        //     System.Console.WriteLine("{0, -3}===\t{1, -20}\t-- {2,-30}\t-- {3,-15}", item.order, item.name, item.email, item.number);
-        // }
-
+        
         for(int i = 0; i < contacts.Count; i++)
         {
             System.Console.WriteLine("{0, -3}===\t{1, -20}\t-- {2,-30}\t-- {3,-15}", contacts[i].order, contacts[i].name, contacts[i].email, contacts[i].number);
@@ -38,15 +41,18 @@ public class Contactlist{
         System.Console.WriteLine("\n");
     }
 
-
+    
+    // Function for searching the contactlist using a given name.
     public void search_Contacts(string name)
     {   
+
         bool found = false;
         for(int i = 0; i < contacts.Count; i++)
         {
             // ToLower is used so that the search is case insensitive and lets the program accept examples like:
             // bEa == bea, true
             // BEa == bea, true
+            // if there exists a name Bea, and is equal to the thrown name in the function, we print their details.
             if(contacts[i].name.ToLower() == name.ToLower())
             {
                 found = true;
@@ -66,17 +72,24 @@ public class Contactlist{
 
     }
 
+
+    // Function for sorting the contactlist.
     public void sort_Contacts(int choice)
     {
         // LINQ based sorting.
         // OrderBy accepts a key from an object x => x.Variable.
-        // ToList() stores the ordered list to a new list on memory.
+        // x.order means it'll use the order and will sort ascending (1 -> 9)
+        // x.name means it'll use the names and will sort descending (a -> z)
+        // ToList() stores the ordered list to a new list on memory. So we equal our current existing "contacts" list to the new, ordered list in the memory.
+        
         if(choice == 1)
             contacts = contacts.OrderBy(x => x.order).ToList();
         if(choice == 2)
             contacts = contacts.OrderBy(x => x.name).ToList();
     }
 
+    
+    // Function for removing contacts.
     public bool remove_Contacts(string name)
     {   
         int index = 0;
@@ -120,6 +133,7 @@ public class Contactlist{
 
     }
 
+    // Function for editing the contact found through name search.
     public void search_edit_Contact(int flag, string name)
     {
         int index = 0;
@@ -131,7 +145,7 @@ public class Contactlist{
             // BEa == bea, true
             if(contacts[i].name.ToLower() == name.ToLower())
             {
-                //this will store the index of the name
+                //this will store the index of the name that we want
                 index = i;
             }
         }
@@ -145,6 +159,7 @@ public class Contactlist{
                 if(input == "0"){
                     return;
                 }
+            // Line below will let us edit the email of the found index.
             contacts[index].email = input;
         }
         else if(flag == 2)
@@ -163,12 +178,15 @@ public class Contactlist{
         }
     }
 
+    // Function for searching using the user's order.
     public void order_search_Contact(int index)
     {
         int new_index = 0;
 
         for(int i = 0; i < contacts.Count; i++)
         {
+            // We add +1 to the index in order to accurately point at the order.
+            // An index of 0 will then return 0+1 = 1 which will point at the user with the order: 1.
             if(contacts[i].order == index+1)
             {
                 new_index = i;
@@ -186,12 +204,17 @@ public class Contactlist{
         System.Console.WriteLine("3. Exit" );
     }
 
+
+    
     public void order_edit_Contact(int index, int flag)
     {
         string input;      
         int new_index = 0;
+        
         for(int i = 0; i < contacts.Count; i++)
         {
+            // We add +1 to the index in order to accurately point at the order.
+            // An index of 0 will then return 0+1 = 1 which will point at the user with the order: 1.
             if(contacts[i].order == index+1)
             {
                 new_index = i;
@@ -226,11 +249,15 @@ public class Contactlist{
 
     }
 
+    // Function for switching orders.
     public void switch_orders(int order_one, int order_two)
     {
         int index_one = 0;
         int index_two = 0;
 
+        // We add +1 to the index in order to accurately point at the order.
+        // An index of 0 will then return 0+1 = 1 which will point at the user with the order: 1.
+        
         //find the first order.
         for(int i = 0; i < contacts.Count; i++)
         {
